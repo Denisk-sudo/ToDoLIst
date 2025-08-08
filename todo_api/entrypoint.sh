2e334000cd3b4d6eb2664919a8d730400e59e044
Denisk-sudo <kostindenis16.04@icloud.com>
2025-08-08 11:08:11 +0300

    1

--- todo_api/entrypoint.sh
+++ todo_api/entrypoint.sh
@@ -8,4 +8,4 @@ echo "🚀 Running Alembic migrations..."
 alembic upgrade head
 
 echo "🚀 Starting backend..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8080
